b0cf214d7dd92bec2b02bdcb4de2c51724b1bbfc
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-05-28 00:31:51 -0300

    Icons

---
+++ src/components/CardModal.tsx
@@ -0,0 +1,9 @@
+export const CardModal = ({info}) => {
+    console.log(info)
+  return (
+    <section>
+        <h2 className="text-white">Card</h2>
+        {info.icono}
+    </section>
+  )
+}
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,37 @@
 import { Dispatch } from "react"
 import { ActionTypes } from "../reducer/actividades-reducer"
+import { CardModal } from "./CardModal"
+import { FaAppleAlt } from "react-icons/fa";
+import { CgGym } from "react-icons/cg";
+import { FaBottleWater } from "react-icons/fa6";
 
 interface ModalProps {
     dispatch: Dispatch<ActionTypes>
 }
-
+const CardModalInfo = [
+    {
+        categoria: "Comida",
+        icono: <FaAppleAlt color="#fff"/>
+    },
+    {
+        categoria: "Agua",
+        icono: <FaBottleWater color="#fff"/>
+    },
+    {
+        categoria: "Ejercicio",
+        icono: <CgGym color="#fff"/>
+    },
+]
 export const Modal = ({ dispatch }: ModalProps) => {
     return (
         <section className="modal-background">
             <p
-            onClick={() => dispatch({type:"[ACTIVAR/DESACTIVAR Moda]",payload:false})}
-             className="modal-close">X</p>
+                onClick={() => dispatch({ type: "[ACTIVAR/DESACTIVAR Moda]", payload: false })}
+                className="modal-close">X</p>
+
+            {
+                CardModalInfo.map((info) => <CardModal info={info}/>)
+            }
         </section>
     )
 }
